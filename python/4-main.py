d91a1a16a7f1d782f4ca8aceacbad340d328af54
KELEBOGILEMOTHOA <mothoakelebogile17@gmail.com>
2024-02-07 12:38:26 +0200

    git first class test

---
+++ python/4-main.py
@@ -0,0 +1,8 @@
+class car:
+    """Represents a car in a car lot"""
+    def __init__(self,make,model,year,price-usd):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.price_usd = price_usd
+
